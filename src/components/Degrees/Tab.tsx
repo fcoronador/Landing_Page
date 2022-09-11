c38d2de12912991a27c10457e0176bce6f90dd7f
fcoronador <ing.ambfredycoronado@gmail.com>
2022-09-11 18:55:23 -0500

    adjust index

--- src/components/Degrees/Tab.tsx
+++ src/components/Degrees/Tab.tsx
@@ -1,11 +1,15 @@
 import { Content } from "./Content"
 
+const generateId = () => {
+    return Math.random().toString(20).substr(2);
+}
+
 export const Tab = ({ degreeSelected }) => {
 
     return (
         <div className="mx-auto mt-5 text-center col-span-5 text-xl">
             {degreeSelected.map((con) => {
-                return <Content content={con} />
+                return <Content key={generateId()} content={con} />
             })}
 
         </div>
