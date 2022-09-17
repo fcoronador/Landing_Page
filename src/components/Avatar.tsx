8aa380fc063a9f53e76822511d306b5410181919
fcoronador <ing.ambfredycoronado@gmail.com>
2022-09-16 19:38:58 -0500

    fix contact

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -7,20 +7,20 @@ export const Avatar = () => {
 
     return (
         <div className='flex'>
-            <div className='mx-auto p-8 self-center'>
-                <img className='mx-auto rounded-full object-fill w-44' src={foto} alt="me_picture" />
+            <div className='mx-auto md:p-8 self-center'>
+                <img className='mx-auto rounded-full object-fill w-20 md:w-44' src={foto} alt="me_picture" />
                 <div className='mt-3  globo iV izquierda-arriba '>
                     {showContact ? (
-                        <div className='flex flex-col justify-center text-center font-["Archivo"] text-base'>
-                            <span>+57 312-415-27-87</span>
-                            <span>ing.ambfredycoronado@gmail.com</span>
-                            <a href="https://drive.google.com/file/d/1fYY07hUxIVQ4r95eO7ZGkCkX3VvAv1-C/view?usp=sharing">
+                        <div className='flex flex-col text-sm md:text-base p-1 text-center font-["Archivo"]'>
+                            <div > +57 312-415-27-87 </div>
+                            <div> ing.ambfredycoronado@gmail.com </div>
+                            <div> <a href="https://drive.google.com/file/d/1fYY07hUxIVQ4r95eO7ZGkCkX3VvAv1-C/view?usp=sharing">
                                 <span>Download CV</span>
                             </a>
-                            <span>Redes Sociales</span>
-                            <span className='font-["BreeSerif"] hover:underline cursor-pointer'  onClick={() => setShowContact(false)}>Hide</span>
+                            </div>
+                            <div><span className='font-["BreeSerif"] hover:underline cursor-pointer' onClick={() => setShowContact(false)}>Hide</span> </div>
                         </div>) : (
-                        <div className=' text-center font-["BreeSerif"] text-xl hover:underline cursor-pointer' onClick={() => setShowContact(true)} >Hi, world click me...</div>)
+                        <div className=' p-1 text-center font-["BreeSerif"] md:text-xl hover:underline cursor-pointer' onClick={() => setShowContact(true)} >Hi, world click me...</div>)
                     }
                 </div>
             </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 export const Header = () => {
     return (
-        <div className="flex flex-col p-8 text-white">
-            <span className='font-["BreeSerif"] text-5xl'>Fredy Esteban</span>
-            <span className='font-["BreeSerif"] text-5xl'>Coronado Romero</span>
-            <span className='font-["Archivo"] text-xl mt-5'>E-Data Engineer</span>
+        <div className="flex flex-col p-2 md:p-8 text-white">
+            <div className='font-["BreeSerif"]  md:text-5xl text-3xl '>Fredy Esteban</div>
+            <div className='font-["BreeSerif"]  md:text-5xl text-3xl'>Coronado Romero</div>
+            <div className='font-["Archivo"]   text-xl mt-5'>E-Data Engineer</div>
         </div>
     )
 }
